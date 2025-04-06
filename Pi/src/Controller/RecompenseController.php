6e2ccc8b959e8cfd94255f1ebf27dbca6a7424b7
mohamedchedlikilani <mohamed.kilani@esprit.tn>
2025-04-06 01:12:54 +0100

    interface Recompense avec controle de saisie

--- Pi/src/Controller/RecompenseController.php
+++ Pi/src/Controller/RecompenseController.php
@@ -4,31 +4,53 @@ namespace App\Controller;
 
 use App\Entity\Recompense;
 use App\Form\RecompenseType;
+use App\Repository\RecompenseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/recompense')]
-final class RecompenseController extends AbstractController
+class RecompenseController extends AbstractController
 {
-    #[Route(name: 'app_recompense_index', methods: ['GET'])]
-    public function index(EntityManagerInterface $entityManager): Response
+    #[Route('/', name: 'app_recompense_index', methods: ['GET'])]
+    public function index(RecompenseRepository $recompenseRepository): Response
     {
-        $recompenses = $entityManager
-            ->getRepository(Recompense::class)
-            ->findAll();
-
         return $this->render('recompense/index.html.twig', [
-            'recompenses' => $recompenses,
+            'recompenses' => $recompenseRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_recompense_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/program/{idprogramme}', name: 'app_recompense_program', methods: ['GET'])]
+    public function programRewards(int $idprogramme, RecompenseRepository $recompenseRepository, EntityManagerInterface $entityManager): Response
+    {
+        $programme = $entityManager->getRepository(\App\Entity\Programmebienetre::class)->find($idprogramme);
+        
+        if (!$programme) {
+            throw $this->createNotFoundException('Program not found');
+        }
+
+        $recompenses = $recompenseRepository->findBy(['idprogramme' => $programme]);
+
+        return $this->render('recompense/program_rewards.html.twig', [
+            'recompenses' => $recompenses,
+            'programme' => $programme,
+        ]);
+    }
+
+    #[Route('/new/{idprogramme}', name: 'app_recompense_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, int $idprogramme): Response
     {
         $recompense = new Recompense();
+        $programme = $entityManager->getRepository(\App\Entity\Programmebienetre::class)->find($idprogramme);
+        
+        if (!$programme) {
+            throw $this->createNotFoundException('Program not found');
+        }
+        
+        $recompense->setIdprogramme($programme);
+        
         $form = $this->createForm(RecompenseType::class, $recompense);
         $form->handleRequest($request);
 
@@ -36,12 +58,13 @@ final class RecompenseController extends AbstractController
             $entityManager->persist($recompense);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_recompense_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_recompense_program', ['idprogramme' => $idprogramme], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('recompense/new.html.twig', [
             'recompense' => $recompense,
             'form' => $form,
+            'programme' => $programme,
         ]);
     }
 
@@ -62,7 +85,7 @@ final class RecompenseController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_recompense_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_programmebienetre_show', ['idprogramme' => $recompense->getIdprogramme()->getIdprogramme()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('recompense/edit.html.twig', [
@@ -71,14 +94,18 @@ final class RecompenseController extends AbstractController
         ]);
     }
 
-    #[Route('/{idrecompense}', name: 'app_recompense_delete', methods: ['POST'])]
+    #[Route('/{idrecompense}/delete', name: 'app_recompense_delete', methods: ['POST'])]
     public function delete(Request $request, Recompense $recompense, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$recompense->getIdrecompense(), $request->getPayload()->getString('_token'))) {
+        $programmeId = $recompense->getIdprogramme()->getIdprogramme();
+        
+        if ($this->isCsrfTokenValid('delete' . $recompense->getIdrecompense(), $request->request->get('_token'))) {
             $entityManager->remove($recompense);
             $entityManager->flush();
+            
+            $this->addFlash('success', 'Reward has been deleted successfully.');
         }
 
-        return $this->redirectToRoute('app_recompense_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_recompense_program', ['idprogramme' => $programmeId], Response::HTTP_SEE_OTHER);
     }
 }
--- Pi/src/Entity/Recompense.php
+++ Pi/src/Entity/Recompense.php
@@ -24,23 +24,23 @@ class Recompense
     /**
      * @var \DateTime|null
      *
-     * @ORM\Column(name="dateAttribution", type="date", nullable=true, options={"default"="NULL"})
+     * @ORM\Column(name="dateAttribution", type="date", nullable=true)
      */
-    private $dateattribution = 'NULL';
+    private $dateattribution = null;
 
     /**
      * @var string|null
      *
-     * @ORM\Column(name="type", type="string", length=255, nullable=true, options={"default"="NULL"})
+     * @ORM\Column(name="type", type="string", length=255, nullable=true)
      */
-    private $type = 'NULL';
+    private $type = null;
 
     /**
      * @var string|null
      *
-     * @ORM\Column(name="statusRecompence", type="string", length=255, nullable=true, options={"default"="NULL"})
+     * @ORM\Column(name="statusRecompence", type="string", length=255, nullable=true)
      */
-    private $statusrecompence = 'NULL';
+    private $statusrecompence = null;
 
     /**
      * @var \Programmebienetre
@@ -52,5 +52,52 @@ class Recompense
      */
     private $idprogramme;
 
+    public function getIdrecompense(): ?int
+    {
+        return $this->idrecompense;
+    }
 
+    public function getDateattribution(): ?\DateTimeInterface
+    {
+        return $this->dateattribution;
+    }
+
+    public function setDateattribution(?\DateTimeInterface $dateattribution): self
+    {
+        $this->dateattribution = $dateattribution;
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): self
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    public function getStatusrecompence(): ?string
+    {
+        return $this->statusrecompence;
+    }
+
+    public function setStatusrecompence(?string $statusrecompence): self
+    {
+        $this->statusrecompence = $statusrecompence;
+        return $this;
+    }
+
+    public function getIdprogramme(): ?\App\Entity\Programmebienetre
+    {
+        return $this->idprogramme;
+    }
+
+    public function setIdprogramme(?\App\Entity\Programmebienetre $idprogramme): self
+    {
+        $this->idprogramme = $idprogramme;
+        return $this;
+    }
 }
