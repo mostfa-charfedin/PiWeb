482fd389c30823bc4fdb0c8fa7a21c752a44749b
souhirkrizi <Souhir.Krizi@esprit.tn>
2025-04-08 04:42:09 +0200

    crx post

--- Pi/migrations/Version20250408021328.php
+++ Pi/migrations/Version20250408021328.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250407230008 extends AbstractMigration
+final class Version20250408021328 extends AbstractMigration
 {
     public function getDescription(): string
     {
