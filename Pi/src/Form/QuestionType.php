cb62538507d6072c0749f8f0f23fbdda62ea5906
{DAGSHUB_USER_NAME} <{DAGSHUB_EMAIL}>
2025-04-14 12:18:50 +0100

    crud done

--- Pi/src/Form/QuestionType.php
+++ Pi/src/Form/QuestionType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\Form\ReponseType;
 
@@ -18,8 +20,15 @@ class QuestionType extends AbstractType
             ->add('question', TextType::class, [
                 'label' => 'Question',
                 'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Question text is required.']),
+                    new Length([
+                        'max' => 80,
+                        'maxMessage' => 'The question cannot exceed 80 characters.',
+                    ]),
+                ],
+                'attr' => ['class' => 'form-control'],
             ])
-            
             ->add('reponses', CollectionType::class, [
                 'entry_type' => ReponseType::class,
                 'allow_add' => true,
--- Pi/src/Form/ReponseType.php
+++ Pi/src/Form/ReponseType.php
@@ -1,14 +1,15 @@
 <?php
 
-
-
 namespace App\Form;
 
 use App\Entity\Reponse;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Form\CallbackTransformer;
 
 class ReponseType extends AbstractType
@@ -16,19 +17,32 @@ class ReponseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('response')
+            ->add('response', TextType::class, [
+                'label' => 'Response Text',
+                'constraints' => [
+                    new NotBlank(['message' => 'Response text is required.']),
+                    new Length([
+                        'max' => 80,
+                        'maxMessage' => 'The response cannot exceed 80 characters.',
+                    ]),
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter response (max 80 characters)',
+                ],
+            ])
             ->add('status', CheckboxType::class, [
                 'label' => 'Correct answer?',
                 'required' => false,
                 'mapped' => true,
             ]);
 
-        // Transform string <=> boolean
+        // Transform string <=> boolean for 'status'
         $builder->get('status')
             ->addModelTransformer(new CallbackTransformer(
-                // transform string (DB) to boolean (form)
+                // Transform from DB string to form boolean
                 fn ($status) => $status === 'correct',
-                // transform boolean (form) to string (DB)
+                // Transform from form boolean to DB string
                 fn ($checked) => $checked ? 'correct' : 'incorrect'
             ));
     }
@@ -40,4 +54,3 @@ class ReponseType extends AbstractType
         ]);
     }
 }
-
