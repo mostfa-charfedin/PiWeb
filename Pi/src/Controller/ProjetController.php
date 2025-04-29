500842952623785b9c33fe96756731d135bd144a
fedi099 <cherni.fedy@gmail.com>
2025-04-29 15:06:31 +0100

    azd

--- Pi/src/Controller/ProjetController.php
+++ Pi/src/Controller/ProjetController.php
@@ -16,6 +16,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use App\Service\ClaudeApiService;
 
 class ProjetController extends AbstractController
 {
